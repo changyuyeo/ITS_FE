d29b5f1620507fae7c4b90667ae3a3b0ee1311b4
여찬규 <wry5533@gmail.com>
2022-08-11 20:19:19 +0900

    refactor: input 컴포넌트 리팩토링 및 스타일 개선

--- packages/withme-ui/src/components/Input/Input.tsx
+++ packages/withme-ui/src/components/Input/Input.tsx
@@ -1,15 +1,19 @@
 import {
 	CSSProperties,
 	FC,
+	FocusEvent,
+	FocusEventHandler,
 	HTMLInputTypeAttribute,
 	InputHTMLAttributes,
 	ReactNode,
 	useCallback,
+	useMemo,
 	useState
 } from 'react';
 import classNames from 'classnames';
 import { SizeTypes } from '../../typings/props.types';
 import { ClosedEye, OpenedEye } from './icons';
+import Text from '../Text';
 
 type InputAttributesType = Omit<InputHTMLAttributes<HTMLInputElement>, 'size' | 'prefix'>;
 type InputSizeType = keyof Pick<SizeTypes, 'sm' | 'base' | 'lg'>;
@@ -17,13 +21,15 @@ type InputSizeType = keyof Pick<SizeTypes, 'sm' | 'base' | 'lg'>;
 interface InputProps extends InputAttributesType {
 	className?: string;
 	error?: boolean;
+	errorMsg?: string;
+	onBlur?: FocusEventHandler<HTMLInputElement>;
+	onFocus?: FocusEventHandler<HTMLInputElement>;
 	password?: boolean;
 	prefix?: ReactNode;
 	size?: InputSizeType;
 	style?: CSSProperties;
 	suffix?: ReactNode;
 	type?: HTMLInputTypeAttribute;
-	value?: string;
 }
 
 const BASE = 'wm-input' as const;
@@ -31,31 +37,67 @@ const BASE = 'wm-input' as const;
 const Input: FC<InputProps> = ({
 	className = '',
 	error = false,
+	errorMsg,
+	onBlur,
+	onFocus,
 	password = false,
 	prefix,
 	size = 'base',
 	style,
 	suffix,
 	type = 'text',
-	value,
 	...props
 }) => {
 	const [hidePassword, setHidePassword] = useState(true);
+	const [focus, setFocus] = useState(false);
+
+	const cx = classNames(
+		BASE,
+		`${BASE}-${size}`,
+		{ [`${BASE}--error`]: error },
+		{ [`${BASE}--focus`]: focus }
+	);
 
 	const onToggleHidePassword = useCallback(() => setHidePassword(prev => !prev), []);
 
-	const cx = classNames(BASE, `${BASE}--size-${size}`, { [`${BASE}--error`]: error });
+	const onBlurHandler = useCallback(
+		(event: FocusEvent<HTMLInputElement>) => {
+			setFocus(false);
+			if (onBlur) onBlur(event);
+		},
+		[onBlur]
+	);
+
+	const onFocusHandler = useCallback(
+		(event: FocusEvent<HTMLInputElement>) => {
+			setFocus(true);
+			if (onFocus) onFocus(event);
+		},
+		[onFocus]
+	);
+
+	const inputStyled: CSSProperties = useMemo(() => ({ position: 'relative', ...style }), [style]);
 
 	return (
-		<div className={`${cx} ${className}`} style={style}>
+		<div className={`${cx} ${className}`} style={inputStyled}>
 			{prefix && <div className={`${BASE}__prefix`}>{prefix}</div>}
-			<input value={value} type={password && hidePassword ? 'password' : type} {...props} />
+			<input
+				type={password && hidePassword ? 'password' : type}
+				onBlur={onBlurHandler}
+				onFocus={onFocusHandler}
+				{...props}
+			/>
 			{password && (
 				<button type="button" className={`${BASE}__password-icon`} onClick={onToggleHidePassword}>
 					{hidePassword ? <OpenedEye /> : <ClosedEye />}
 				</button>
 			)}
 			{suffix && <div className={`${BASE}__suffix`}>{suffix}</div>}
+			{errorMsg && error && (
+				<Text size="xs" color="error" className={`${BASE}__message`}>
+					{errorMsg}
+				</Text>
+			)}
 		</div>
 	);
 };
--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,17 +1,34 @@
-import { Container, Wrapper } from './components/styled';
-import { Divider, Input, Text, Title } from '@with-me/ui';
 import { ChangeEvent, useState } from 'react';
-// import ButtonTest from './components/ButtonTest';
-// import SpinnerTest from './components/SpinnerTest';
+import { Button, Input } from '@with-me/ui';
+import { Container, Wrapper } from './styled';
 
 const App = () => {
 	const [value, setValue] = useState('');
+	const [error, setError] = useState(false);
+
 	const onChangeValue = (e: ChangeEvent<HTMLInputElement>) => setValue(e.target.value);
 
 	return (
 		<Container>
-			<Input size="base" value={value} onChange={onChangeValue} />
+			<Button type="error" onClick={() => setError(prev => !prev)}>
+				Error!
+			</Button>
+			<Wrapper>
+				<Input size="sm" value={value} onChange={onChangeValue} />
+				<Input size="base" value={value} onChange={onChangeValue} />
+				<Input size="lg" value={value} onChange={onChangeValue} />
+			</Wrapper>
 			<Input placeholder="입력해주세요" value={value} onChange={onChangeValue} />
+			<br />
+			<Input
+				placeholder="에러 반응!"
+				error={error}
+				errorMsg={'응애'}
+				value={value}
+				onChange={onChangeValue}
+			/>
+			<br />
+			<Input placeholder="비밀번호" password />
 		</Container>
 	);
 };
