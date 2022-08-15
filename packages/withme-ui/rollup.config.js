63222770e71ad13c61d6c2d7565c50c9874a5efc
여찬규 <wry5533@gmail.com>
2022-08-15 20:02:07 +0900

    build: 컴포넌트 패키지 트리쉐이킹 설정 및 d.ts 적용

--- packages/withme-ui/rollup.config.js
+++ packages/withme-ui/rollup.config.js
@@ -1,24 +1,26 @@
 import typescript from 'rollup-plugin-typescript2';
 import dts from 'rollup-plugin-dts';
+import multiInput from 'rollup-plugin-multi-input';
 import commonjs from '@rollup/plugin-commonjs';
 
+
 export default [
-	// js 번들링
+	  // tree shaking 을 위해 esm 파일들을 code splitting 하여 빌드
 	{
-		input: 'src/index.ts',
+		input: ['src/**/index.ts', 'src/**/index.tsx'],
 		output: [
-			{ dir: 'build', format: 'cjs', sourcemap: true, exports: 'auto' },
-			{ dir: 'build/esm', format: 'esm', sourcemap: true }
+			{ dir: './build', format: 'cjs', sourcemap: true, exports: 'auto' },
+			{ dir: './build/esm', format: 'esm', sourcemap: true }
 		],
-		plugins: [commonjs(), typescript()],
+		plugins: [commonjs(), multiInput(), typescript()],
 		preserveModules: true,
 		external: ['react', 'classnames', 'react/jsx-runtime']
 	},
-	// d.ts (타입 정의 파일) 번들링
+	// d.ts (타입 정의 파일) 빌드
 	{
 		input: 'src/typings/props.types.d.ts',
 		output: [
-			{ file: 'build/typings/props.types.d.ts', format: 'cjs', sourcemap: true, exports: 'auto' },
+			{ file: 'build/typings/props.types.d.ts', format: 'cjs', sourcemap: true },
 			{ file: 'build/esm/typings/props.types.d.ts', format: 'esm', sourcemap: true }
 		],
 		plugins: [dts()]
