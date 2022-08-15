1500d4e72e3e76459d471f9a496609e0510039b4
여찬규 <wry5533@gmail.com>
2022-08-15 22:22:28 +0900

    chore: chore

--- packages/withme-icon/rollup.config.js
+++ packages/withme-icon/rollup.config.js
@@ -1,13 +1,14 @@
 import typescript from 'rollup-plugin-typescript2';
+import multiInput from 'rollup-plugin-multi-input';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-	input: 'src/index.ts',
+	input: ['src/**/index.ts', 'src/**/index.tsx'],
 	output: [
-		{ dir: 'build', format: 'cjs', sourcemap: true, exports: 'auto' },
-		{ dir: 'build/esm', format: 'esm', sourcemap: true }
+		{ dir: './build', format: 'cjs', sourcemap: true, exports: 'auto' },
+		{ dir: './build/esm', format: 'esm', sourcemap: true }
 	],
-	plugins: [commonjs(), typescript()],
+	plugins: [commonjs(), multiInput(), typescript()],
 	preserveModules: true,
 	external: ['react', 'react/jsx-runtime']
 };
