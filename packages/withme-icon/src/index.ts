35851dce2bb1f4d72fad7c0ce6333541d7b0117c
여찬규 <wry5533@gmail.com>
2022-08-15 22:15:11 +0900

    fix: icon 패키지 타입에러 해결

--- packages/withme-icon/src/index.ts
+++ packages/withme-icon/src/index.ts
@@ -1,2 +1,11 @@
-export * from './common';
-// export * from './skills';
+// common
+export { default as ArrowDown } from './common/ArrowDown';
+export { default as ArrowLeft } from './common/ArrowLeft';
+export { default as ArrowRight } from './common/ArrowRight';
+export { default as ArrowUp } from './common/ArrowUp';
+export { default as BackSpace } from './common/BackSpace';
+export { default as Close } from './common/Close';
+export { default as Code } from './common/Code';
+export { default as Github } from './common/Github';
+export { default as Heart } from './common/Heart';
+export { default as Sharing } from './common/Sharing';
