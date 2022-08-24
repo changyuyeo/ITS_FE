a50d567253dcd5ae84681fa55a18b482670e265f
여찬규 <wry5533@gmail.com>
2022-08-24 23:24:30 +0900

    feat: card component 스토리 문서 작성

--- packages/withme-ui/.storybook/main.js
+++ packages/withme-ui/.storybook/main.js
@@ -3,6 +3,7 @@ module.exports = {
 		// '../src/components/**/*.stories.mdx',
 		'../src/components/Avatar/Avatar.stories.mdx',
 		'../src/components/Button/Button.stories.mdx',
+		'../src/components/Card/Card.stories.mdx',
 		'../src/components/Icons/Icons.stories.mdx'
 	],
 	addons: [
