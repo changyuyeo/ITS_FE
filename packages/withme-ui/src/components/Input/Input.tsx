3cacaa6c5174324d66acbd4e5e131c4c89429273
여찬규 <wry5533@gmail.com>
2022-09-02 20:19:19 +0900

    feat: input 컴포넌트 forwardRef 적용

--- packages/withme-ui/src/components/Input/Input.tsx
+++ packages/withme-ui/src/components/Input/Input.tsx
@@ -1,8 +1,8 @@
 import {
 	CSSProperties,
-	FC,
 	FocusEvent,
 	FocusEventHandler,
+	forwardRef,
 	HTMLInputTypeAttribute,
 	InputHTMLAttributes,
 	ReactNode,
@@ -14,10 +14,7 @@ import classNames from 'classnames';
 import type { SizeTypes } from '../../typings/props.types';
 import Text from '../Text';
 
-type InputAttributesType = Omit<InputHTMLAttributes<HTMLInputElement>, 'size' | 'prefix'>;
-type InputSizeType = keyof Pick<SizeTypes, 'sm' | 'base' | 'lg'>;
-
-interface InputProps extends InputAttributesType {
+interface InputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'size' | 'prefix'> {
 	className?: string;
 	error?: boolean;
 	errorMsg?: string;
@@ -25,86 +22,13 @@ interface InputProps extends InputAttributesType {
 	onFocus?: FocusEventHandler<HTMLInputElement>;
 	password?: boolean;
 	prefix?: ReactNode;
-	size?: InputSizeType;
+	size?: keyof Pick<SizeTypes, 'sm' | 'base' | 'lg'>;
 	style?: CSSProperties;
 	suffix?: ReactNode;
 	type?: HTMLInputTypeAttribute;
 }
 
 const BASE = 'wm-input' as const;
-
-const Input: FC<InputProps> = ({
-	className = '',
-	error = false,
-	errorMsg,
-	onBlur,
-	onFocus,
-	password = false,
-	prefix,
-	size = 'base',
-	style,
-	suffix,
-	type = 'text',
-	...props
-}) => {
-	const [hidePassword, setHidePassword] = useState(true);
-	const [focus, setFocus] = useState(false);
-
-	const cx = classNames(
-		BASE,
-		`${BASE}-${size}`,
-		{ [`${BASE}--error`]: error },
-		{ [`${BASE}--focus`]: focus }
-	);
-
-	const onToggleHidePassword = useCallback(() => setHidePassword(prev => !prev), []);
-
-	const onBlurHandler = useCallback(
-		(event: FocusEvent<HTMLInputElement>) => {
-			setFocus(false);
-			if (onBlur) onBlur(event);
-		},
-		[onBlur]
-	);
-
-	const onFocusHandler = useCallback(
-		(event: FocusEvent<HTMLInputElement>) => {
-			setFocus(true);
-			if (onFocus) onFocus(event);
-		},
-		[onFocus]
-	);
-
-	const inputStyled: CSSProperties = useMemo(() => ({ position: 'relative', ...style }), [style]);
-
-	return (
-		<div className={`${cx} ${className}`} style={inputStyled}>
-			{prefix && <div className={`${BASE}__prefix`}>{prefix}</div>}
-			<input
-				type={password && hidePassword ? 'password' : type}
-				onBlur={onBlurHandler}
-				onFocus={onFocusHandler}
-				{...props}
-			/>
-			{password && (
-				<button type="button" className={`${BASE}__password-icon`} onClick={onToggleHidePassword}>
-					{
-						// eslint-disable-next-line @typescript-eslint/no-use-before-define
-						hidePassword ? <OpenedEye /> : <ClosedEye />
-					}
-				</button>
-			)}
-			{suffix && <div className={`${BASE}__suffix`}>{suffix}</div>}
-			{errorMsg && error && (
-				<Text size="xs" color="error" className={`${BASE}__message`}>
-					{errorMsg}
-				</Text>
-			)}
-		</div>
-	);
-};
-
-export default Input;
 
 const ClosedEye = () => (
 	<svg
@@ -139,3 +63,78 @@ const OpenedEye = () => (
 		/>
 	</svg>
 );
+
+const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+	const {
+		className = '',
+		error = false,
+		errorMsg,
+		onBlur,
+		onFocus,
+		password = false,
+		prefix,
+		size = 'base',
+		style,
+		suffix,
+		type = 'text',
+		...rest
+	} = props;
+
+	const [hidePassword, setHidePassword] = useState(true);
+	const [focus, setFocus] = useState(false);
+
+	const cx = classNames(
+		BASE,
+		`${BASE}-${size}`,
+		{ [`${BASE}--error`]: error },
+		{ [`${BASE}--focus`]: focus }
+	);
+
+	const onToggleHidePassword = useCallback(() => setHidePassword(prev => !prev), []);
+
+	const onBlurHandler = useCallback(
+		(event: FocusEvent<HTMLInputElement>) => {
+			setFocus(false);
+			if (onBlur) onBlur(event);
+		},
+		[onBlur]
+	);
+
+	const onFocusHandler = useCallback(
+		(event: FocusEvent<HTMLInputElement>) => {
+			setFocus(true);
+			if (onFocus) onFocus(event);
+		},
+		[onFocus]
+	);
+
+	const inputStyled: CSSProperties = useMemo(() => ({ position: 'relative', ...style }), [style]);
+
+	return (
+		<div className={`${cx} ${className}`} style={inputStyled}>
+			{prefix && <div className={`${BASE}__prefix`}>{prefix}</div>}
+			<input
+				ref={ref}
+				type={password && hidePassword ? 'password' : type}
+				onBlur={onBlurHandler}
+				onFocus={onFocusHandler}
+				{...rest}
+			/>
+			{password && (
+				<button type="button" className={`${BASE}__password-icon`} onClick={onToggleHidePassword}>
+					{hidePassword ? <OpenedEye /> : <ClosedEye />}
+				</button>
+			)}
+			{suffix && <div className={`${BASE}__suffix`}>{suffix}</div>}
+			{errorMsg && error && (
+				<Text size="xs" color="error" className={`${BASE}__message`}>
+					{errorMsg}
+				</Text>
+			)}
+		</div>
+	);
+});
+
+Input.displayName = 'Input';
+
+export default Input;
--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,27 +1,37 @@
 import { Container } from './styled';
-import { Button, Textarea } from '@with-me/ui';
-import { ChangeEvent, useRef, useState } from 'react';
+import { Button, Input, Textarea } from '@with-me/ui';
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
 
 const App = () => {
-	const textAreaRef = useRef<HTMLTextAreaElement>(null);
-	const buttonRef = useRef<HTMLButtonElement>(null);
+	// const textAreaRef = useRef<HTMLTextAreaElement>(null);
+	// const buttonRef = useRef<HTMLButtonElement>(null);
+	const inputRef = useRef<HTMLInputElement>(null);
+	const inputRef2 = useRef<HTMLInputElement>(null);
 
-	const [value, setValue] = useState('');
+	// const [value, setValue] = useState('');
 
-	const onChange = (e: ChangeEvent<HTMLTextAreaElement>) => setValue(e.target.value);
+	// const onChange = (e: ChangeEvent<HTMLTextAreaElement>) => setValue(e.target.value);
 
-	// const onClick = () => {
-	// 	setValue('');
-	// 	if (ref) ref.current?.focus();
-	// };
+	// // const onClick = () => {
+	// // 	setValue('');
+	// // 	if (ref) ref.current?.focus();
+	// // };
+
+	useEffect(() => {
+		// inputRef.current?.focus();
+		inputRef2.current?.focus();
+	}, []);
 
 	return (
 		<Container>
-			<Textarea value={value} onChange={onChange} />
+			{/* <Textarea value={value} onChange={onChange} />
 			<Button ref={buttonRef} type="error" onClick={() => console.log('ref test!')}>
 				Delete
 			</Button>
-			<Button onClick={() => buttonRef.current && buttonRef.current.click()}>Test</Button>
+			<Button onClick={() => buttonRef.current && buttonRef.current.click()}>Test</Button> */}
+
+			<Input ref={inputRef2} />
+			<Input ref={inputRef} />
 		</Container>
 	);
 };
